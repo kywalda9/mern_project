18c490b1e905e2a51874fb408a06051426873250
Bohn <y2742@softwareag.com>
2020-06-16 00:46:11 +0200

    Add User Places

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import {
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UsersList from './user/components/UsersList';
 
 const App = () => {
   return (
@@ -18,6 +20,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
