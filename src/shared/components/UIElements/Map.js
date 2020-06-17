62d01ec03eae953cc3df5b536b2f8e9fb15a4317
Bohn <y2742@softwareag.com>
2020-06-17 18:05:13 +0200

    Show Map

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./Map.css";
+
+const Map = () => {
+  return;
+  <div className={`map ${props.className}`} style={props.style}></div>;
+};
+
+export default Map;
---
+++ src/shared/components/UIElements/Modal.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+
+import Backdrop from "./Backdrop";
+import "./Modal.css";
+
+const ModalOverlay = (props) => {
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.OnSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        {/* Interactive elements often have a standard browser behavior. 
+                For example, a link navigates to another resource, links and buttons can be reached with the Tab key (↹ ) and then receive the focus. 
+                If the event is cancelable, this default action can be aborted with the Event.preventDefault() method.             */}
+        <div className={`modal__content ${props.content}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
+};
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </React.Fragment>
+  );
+};
+
+export default Modal;
